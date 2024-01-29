c5ce43a42c4197221ea77eaf44d24de7c21a5085
Garv <garvgoswami@berkeley.edu>
2024-01-29 01:03:11 -0800

    color changes - dropdown add

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,120 +4,81 @@ import Link from 'next/link'
 import Image from 'next/image'
 const Home = () => {
     const [hasAnswered, setHasAnswered] = useState(false)
+    const [isDropdownVisible, setDropdownVisibility] = useState(false);
+    
+    const toggleDropdown = () => {
+        setDropdownVisibility(!isDropdownVisible);
+    }
+    
+
     return (
 
-        <div className='h-screen bg-white text-black flex'>
+
+  
+      /* Outer container div for full page */
+
+    <div style={{ backgroundColor: '#F9F6EE' }} className='h-screen text-black flex'>
+
+      {/* sidebar div*/}
+      {/*sidebar extends full height of screen and is using rounded property because I'm trying to overlap it with the top nav*/}
+
+      
+  <div className='mt-16 w-42 overflow-y-auto bg-black pt-5 rounded-tr-3xl' style={{ backgroundColor: '#bdedf0' }}>
+    {/* Dropdown Menu */}
+
+      <button style = {{backgroundColor: '#FFA338'}}  className= 'flex items-center text-white p-2 rounded mt-44 onClick = {toggleDropdown}'>
+        State
+        <svg className="w-4 h-4 ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 011.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+        </svg>
+      </button>
+      <ul className={`absolute left-0 mt-2 p-2 bg-white border rounded ${isDropdownVisible ? '' : 'hidden'}`}>
+        <li><a href="#">Dropdown Item 1</a></li>
+        <li><a href="#">Dropdown Item 2</a></li>
+      </ul>
+
+      <button style = {{backgroundColor: '#FFA338'}}  className= 'flex items-center text-white p-2 rounded mt-44 onClick = {toggleDropdown}'>
+        Document Type
+        <svg className="w-4 h-4 ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 011.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+        </svg>
+      </button>
+
+      <button style = {{backgroundColor: '#FFA338'}}  className= 'flex items-center text-white p-2 rounded mt-44 onClick = {toggleDropdown}'>
+        Payer
+        <svg className="w-4 h-4 ml-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+          <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 011.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
+        </svg>
+      </button>
+
+
+    {/* End of Sidebar content */}
+  </div>
+
+  <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-300 w-5/6 h-1"></div>
 
             
-
-            <div className='w-28 flex flex-col'>
-                <div className='relative flex flex-col flex-grow overflow-y-auto bg-black pt-5' style={{ backgroundColor: '#236194' }} >
-                
-                    <div className='absolute bottom-0 inset-x-0 border-t border-gray-200/50 mx-2 py-6 px-2'>
-                        
-                    </div>
-
-                </div>
-            </div>
-
-            
-        
-
-            <div className='relative flex flex-1 flex-col h-full'>
+               <div className='relative flex flex-1 flex-col h-full'>
                 {!hasAnswered && <div className='flex flex-col space-y-4 justify-center items-center absolute inset-x-0 top-0 bottom-0'>
-                    <img src = 'demo-ui/public/policyreporter.svg' />
+
                 </div>}
 
-                <nav className="bg-white border-gray-200 dark:bg-gray-900">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="https://policyreporter.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-        <img src="./public/vercel.svg" className="h-8" alt="Policy Reporter Logo" />
-        <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Policy Reporter</span>
-    </a>
-    <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span class="sr-only">Open main menu</span>
-        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-    <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-          <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-        </li>
-        <li>
-          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-        </li>
+
+
+
+                <div className="bg-white border-gray-200 rounded-bl-3xl" style={{ backgroundColor: '#22A9B2' }}>
+  <div className="w- flex flex-wrap items-center justify-between mx-auto p-5">
+    <Image src="./public/cairlogonobg.svg" width={30} height={30} />
+    <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
+    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+      <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        {/* Add your list items here */}
       </ul>
     </div>
   </div>
-</nav>
-
-
-
-//Dropdown
-<div className="relative" data-te-dropdown-ref>
-  <button
-    className="flex items-center whitespace-nowrap rounded bg-primary px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] motion-reduce:transition-none dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-    type="button"
-    id="dropdownMenuButton1"
-    data-te-dropdown-toggle-ref
-    aria-expanded="false"
-    data-te-ripple-init
-    data-te-ripple-color="light">
-    Dropdown button
-    <span className="ml-2 w-2">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 20 20"
-        fill="black"
-        className="h-5 w-5">
-        <path
-          fill-rule="evenodd"
-          d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-          clip-rule="evenodd" />
-      </svg>
-    </span>
-  </button>
-  <ul
-    class="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
-    aria-labelledby="dropdownMenuButton1"
-    data-te-dropdown-menu-ref>
-    <li>
-      <a
-        class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-        href="#"
-        data-te-dropdown-item-ref
-        >Action</a
-      >
-    </li>
-    <li>
-      <a
-        class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-        href="#"
-        data-te-dropdown-item-ref
-        >Another action</a
-      >
-    </li>
-    <li>
-      <a
-        class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-        href="#"
-        data-te-dropdown-item-ref
-        >Something else here</a
-      >
-    </li>
-  </ul>
 </div>
+
+
 
 
                 {hasAnswered && <div className='flex flex-col bg-white text-black'>
@@ -154,6 +115,7 @@ const Home = () => {
                 </div>
             </div>
         </div>
+
     )
 }
 
