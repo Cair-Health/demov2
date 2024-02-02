312d30db4a47864d6be9b37ad279aacd6ecf6efc
Garv <garvgoswami@berkeley.edu>
2024-02-02 13:21:26 -0800

    profile

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,6 +8,7 @@ import PersonIcon from "/images/person.svg"
 import HomeIcon from "/images/home.svg"
 
 import CairLogo from "/images/CairHealthLogo.png"
+import logo from "/images/logo.png"
 
 const Home = () => {
     const [hasAnswered, setHasAnswered] = useState(false)
@@ -188,21 +189,21 @@ const Home = () => {
 
  {hasAnswered && <div className='flex flex-col text-black'>
                     <div className='w-full flex items-center justify-center'>
-                        <div className='flex space-x-4 items-center justify-between px-6 py-6 w-3/4'>
+                        <div className='flex space-x-4 items-center justify-between px-6 py-6 w-4/5'>
                             <div className='flex space-x-4 items-center'>
                                 <div className='h-8 w-6 bg-indigo-500 text-center p-1 px-2 rounded text-white'>P</div>
-                                <p style={{ fontSize: '20px', fontFamily: 'serif' }}>How does this work</p>
+                                <p style={{ fontSize: '20px', fontFamily: 'Quicksand, sans-serif' }}>How does this work</p>
                             </div>
                             <PencilSquareIcon className='h-6 w-6' />
                         </div>
                     </div>
                     <div className='w-full flex items-center justify-center border-t border-b' style = {{background: '#FAF9F6'}}>
-                        <div className='flex space-x-4 items-center justify-between px-6 py-6 w-3/4'>
+                        <div className='flex space-x-4 items-center justify-between px-6 py-6 w-4/5'>
                             <div className='flex space-x-4 items-center'>
-                                <div className='h-8 w-4 bg-teal-600 text-center p-2 rounded text-white relative'>
-                                    <Image src="/CairHealthLogo.svg" fill alt='none' />
+                                <div className='h- w-30 bg-teal-600 text-center p-2 rounded text-white relative'>
+                                    <h1>{'{ai}'}</h1>
                                 </div>
-                                <p style={{ fontSize: '20px', fontFamily: 'serif' }}>I'm assuming you're referring to how I work as a language model. As an AI language model, I was trained using vast amounts of data from the internet, books, and other sources. My training involved analyzing this data to identify patterns and relationships between words and phrases, as well as understanding the structure of language itself.
+                                <p style={{ fontSize: '20px', fontFamily: 'Quicksand, sans-serif' }}>I'm assuming you're referring to how I work as a language model. As an AI language model, I was trained using vast amounts of data from the internet, books, and other sources. My training involved analyzing this data to identify patterns and relationships between words and phrases, as well as understanding the structure of language itself.
 
                                     When you ask me a question or provide me with a prompt, I use my knowledge of language to generate a response that is relevant and meaningful. I do this by using a complex algorithm</p>
                             </div>
